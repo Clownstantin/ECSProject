6ac6b9522d8c0b68bb83fac5612c9592d90399d1
Clownstantine <constantin2033@gmail.com>
2022-12-28 05:35:01 +0300

    Binary room generation.

--- Assets/Scripts/Global/Common/Extension/ProceduralGenerationAlgorithms.cs
+++ Assets/Scripts/Global/Common/Extension/ProceduralGenerationAlgorithms.cs
@@ -31,5 +31,72 @@ namespace RougeLike
 			}
 			return corridorPositions;
 		}
+
+		public static List<BoundsInt> BinarySpacePartitioning(BoundsInt spaceToSplit, int minWidth, int minHeight)
+		{
+			var roomsQueue = new Queue<BoundsInt>();
+			var roomsList = new List<BoundsInt>();
+			roomsQueue.Enqueue(spaceToSplit);
+
+			while(roomsQueue.Count > 0)
+			{
+				BoundsInt roomBounds = roomsQueue.Dequeue();
+				if(roomBounds.size.x < minWidth || roomBounds.size.y < minHeight) continue;
+
+				if(Random.value < 0.5f)
+				{
+					if(roomBounds.size.y >= minHeight * 2)
+						SplitHorizontally(minHeight, roomsQueue, roomBounds);
+					else if(roomBounds.size.x >= minWidth * 2)
+						SplitVertically(minWidth, roomsQueue, roomBounds);
+					else
+						roomsList.Add(roomBounds);
+				}
+				else
+				{
+					if(roomBounds.size.x >= minWidth * 2)
+						SplitVertically(minWidth, roomsQueue, roomBounds);
+					else if(roomBounds.size.y >= minHeight * 2)
+						SplitHorizontally(minHeight, roomsQueue, roomBounds);
+					else
+						roomsList.Add(roomBounds);
+				}
+			}
+			return roomsList;
+		}
+
+		private static void SplitVertically(int minWidth, Queue<BoundsInt> roomsQueue, BoundsInt roomBounds)
+		{
+			Vector3Int roomSize = roomBounds.size;
+			Vector3Int room1Pos = roomBounds.min;
+
+			int xSplit = Random.Range(1, roomSize.x);
+			var room1Size = new Vector3Int(xSplit, roomSize.y, roomSize.z);
+			var room2Pos = new Vector3Int(room1Pos.x + xSplit, room1Pos.y, room1Pos.z);
+			var room2Size = new Vector3Int(roomSize.x - xSplit, roomSize.y, roomSize.z);
+
+			var room1 = new BoundsInt(room1Pos, room1Size);
+			var room2 = new BoundsInt(room2Pos, room2Size);
+
+			roomsQueue.Enqueue(room1);
+			roomsQueue.Enqueue(room2);
+		}
+
+		private static void SplitHorizontally(int minHeight, Queue<BoundsInt> roomsQueue, BoundsInt roomBounds)
+		{
+			Vector3Int roomSize = roomBounds.size;
+			Vector3Int room1Pos = roomBounds.min;
+
+			int ySplit = Random.Range(1, roomSize.y);
+			var room1Size = new Vector3Int(roomSize.x, ySplit, roomSize.z);
+			var room2Pos = new Vector3Int(room1Pos.x, room1Pos.y + ySplit, room1Pos.z);
+			var room2Size = new Vector3Int(roomSize.x, roomSize.y - ySplit, roomSize.z);
+
+			var room1 = new BoundsInt(room1Pos, room1Size);
+			var room2 = new BoundsInt(room2Pos, room2Size);
+
+			roomsQueue.Enqueue(room1);
+			roomsQueue.Enqueue(room2);
+		}
 	}
 }
---
+++ Assets/Scripts/Global/Common/UnityComponents/Generation/RoomFirstGenerator.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace RougeLike
+{
+	public class RoomFirstGenerator : RandomWalkGenerator
+	{
+		[SerializeField] private int _minRoomWidth, _minRoomHeight;
+		[SerializeField] private int _dungeonWidth, _dungeonHeight;
+		[SerializeField, Range(0, 10)] private int _offset = 1;
+		[SerializeField] private bool _isRandomWalkRooms = false;
+
+		protected override void RunProceduralGeneration() => CreateRooms();
+
+		private void CreateRooms()
+		{
+			var totalSize = new Vector3Int(_dungeonWidth, _dungeonHeight);
+			var totalBounds = new BoundsInt((Vector3Int)startPosition, totalSize);
+			List<BoundsInt> rooms = ProceduralGenerationAlgorithms.BinarySpacePartitioning(totalBounds, _minRoomWidth, _minRoomHeight);
+			HashSet<Vector2Int> floor = CreateSimpleRooms(rooms);
+
+			visualizer.PaintFloorTiles(floor);
+			visualizer.PaintWallTiles(WallGenerator.GetGeneratedWallPositions(floor));
+		}
+
+		private HashSet<Vector2Int> CreateSimpleRooms(List<BoundsInt> rooms)
+		{
+			var floor = new HashSet<Vector2Int>();
+			foreach(BoundsInt room in rooms)
+			{
+				for(int col = 0; col < room.size.x - _offset; col++)
+				{
+					for(int row = _offset; row < room.size.y - _offset; row++)
+					{
+						Vector2Int pos = (Vector2Int)room.min + new Vector2Int(col, row);
+						floor.Add(pos);
+					}
+				}
+			}
+			return floor;
+		}
+	}
+}
